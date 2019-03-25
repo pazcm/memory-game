3433bdd9bfd6af3e1a233dd7014d780971df11d9
Marta Paz <pazmarta@gmail.com>
2019-03-25 18:28:21 +0000

    add structure, start button, display cards

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+var cards = [1, 2, 3, 4, 5, 6, 7, 8];
+
+var game = document.getElementById('game');
+var board = document.createElement('div');
+var startButton = document.getElementById('start');
+var displayGame = game.style.display;
+
+board.setAttribute('id', 'board');
+board.setAttribute('class', 'box board');
+game.insertBefore(board, game.children[3]);
+
+cards.forEach(item => {
+  var card = document.createElement('div');
+  card.classList.add('card');
+  card.style.border = "thick solid blue";
+  board.appendChild(card);
+});
+
+startButton.onclick = function(){
+    game.style.display ='grid';
+    startButton.remove();
+    [].forEach.call( document.getElementsByClassName( 'card' ), function ( card, i ) {
+        card.textContent = cards[i];
+    });
+}
